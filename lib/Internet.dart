2e358737194af9fd9aeff126df1e9f8aa6add751
h3ck3r <82164995+rSrikesh@users.noreply.github.com>
2023-07-13 20:47:39 +0530

    worked on Internet and Sandbox module

--- lib/Internet.dart
+++ lib/Internet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:requests/requests.dart';
 
 class Internet extends StatefulWidget {
   const Internet({super.key});
@@ -9,25 +10,60 @@ class Internet extends StatefulWidget {
 }
 
 class _State extends State<Internet> {
+  TextEditingController x = TextEditingController();
+  TextEditingController y = TextEditingController();
+  bool isClicked = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           children: <Widget>[
+            const SizedBox(height: 100.0),
+            Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextField(
+                  controller: x,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter First Value'),
+                )),
+            const SizedBox(height: 20.0),
+            Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextField(
+                  controller: y,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter Second Value'),
+                )),
             TextButton(
-              onPressed: (){},
-              child: const Text('HTTPS')
-            ),
-
+                onPressed: () async {
+                  var r = await Requests.get(
+                      'https://8ksec.io/?val=${x.text}${y.text}');
+                  setState(() {
+                    isClicked = true;
+                  });
+                },
+                child: const Text('HTTPS')),
             TextButton(
-              onPressed: (){},
-              child: const Text('HTTP')
-            ),
-
-            TextButton(
-              onPressed: (){},
-              child: const Text('Pinned')
+                onPressed: () async {
+                  var r = await Requests.get(
+                      'http://example.com/?val=${x.text}${y.text}');
+                  setState(() {
+                    isClicked = true;
+                  });
+                },
+                child: const Text('HTTP')),
+            TextButton(onPressed: () {}, child: const Text('Pinned')),
+            const SizedBox(height: 15.0),
+            Visibility(
+              visible: isClicked,
+              child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Text('Value sent is ${x.text + y.text}',
+                      style: const TextStyle(fontSize: 20))),
             ),
           ],
         ),
--- lib/Sandbox.dart
+++ lib/Sandbox.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Sandbox extends StatefulWidget {
   const Sandbox({super.key});
@@ -9,12 +10,60 @@ class Sandbox extends StatefulWidget {
 }
 
 class _State extends State<Sandbox> {
+  TextEditingController a = TextEditingController();
+  TextEditingController b = TextEditingController();  
+  late SharedPreferences prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeSharedPreferences();
+  }
+
+  Future<void> initializeSharedPreferences() async {
+    prefs = await SharedPreferences.getInstance();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
-          
+          children: <Widget>[
+            const SizedBox(height: 100.0),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextField(
+                controller: a,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter First Value'),
+              ),
+            ),
+
+            const SizedBox(height: 15.0),
+
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextField(
+                controller: b,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter Second Value'
+                ),
+              )
+            ),
+
+            const SizedBox(height: 15.0),
+            
+            TextButton(
+              onPressed: () async{
+                await prefs.setString('value_stored',a.text);
+
+              },
+              child: const Text('Submit')
+            ),
+          ],
         ),
       ),
     );
